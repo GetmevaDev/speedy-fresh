df12e25afa739d55d715715f601dcf9373fd724a
record001 <umarkhidirboev@gmail.com>
2023-06-15 23:43:07 +0500

    change one img

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Hero from "../components/Hero";
 import "../css/map.css"
-import hero_img from "../images/african-american-y.jpg";
+import hero_img from "../images/african-american-y.webp";
 
 import address from "../images/icon-address.svg";
 import hours from "../images/icon-hours.svg";
